d736c757009d695fbcdaa62c9ca98e37f8d3fe2e
Diogooliveira10 <ddiogooliveira9@gmail.com>
2025-03-30 00:17:09 -0300

    fix: bug in code

--- tracing.js
+++ tracing.js
@@ -1,25 +1,36 @@
-const { NodeSDK } = require('@opentelemetry/sdk-node')
-const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node')
-const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http')
+const { NodeSDK } = require('@opentelemetry/sdk-node') // Importa o SDK do OpenTelemetry para Node.js
+const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node') // Auto-instrumentações para detectar requisições HTTP, DB, etc.
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http')// Exportador para enviar os traces para o OTEL Collector via HTTP
 
 const traceExporter = new OTLPTraceExporter({
-    url: 'http://localhost:4318/v1/traces' // Endpoint do OTEL Collector
+    url: 'http://localhost:4318/v1/traces', // Endpoint do OTEL Collector
 })
 
 const sdk = new NodeSDK({
-    traceExporter,
-    instrumentations: [getNodeAutoInstrumentations()]
+    traceExporter, // Exportador de traces
+    instrumentations: [getNodeAutoInstrumentations()] // Instrumentações automáticas para monitorar requisições HTTP, DB, etc.
 })
 
-// Inicializa OpenTelemetry
-sdk.start()
-    .then(() => console.log('OpenTelemetry iniciado!'))
-    .catch(err => console.error('Erro ao iniciar OpenTelemetry', err))
+// Inicializa o OpenTelemetry de forma assíncrona
+async function startOpenTelemetry() {
+    try {
+        await sdk.start() // Inicia o OpenTelemetry para capturar traces
+        console.log('OpenTelemetry started!')
+    } catch (err) {
+        console.error('Error starting OpenTelemetry:', err)
+    }
+}
+
+startOpenTelemetry();
 
 // Fecha o SDK corretamente ao encerrar a aplicação
-process.on('SIGTERM', () => {
-    sdk.shutdown()
-        .then(() => console.log('OpenTelemetry finalizado!'))
-        .catch(err => console.error('Erro ao finalizar OpenTelemetry', err))
-        .finally(() => process.exit(0))
-})
+process.on('SIGTERM', async () => {
+    try {
+        await sdk.shutdown() // Encerra o OpenTelemetry e exporta todos os dados pendentes
+        console.log('OpenTelemetry finished!')
+    } catch (err) {
+        console.error('Error terminating OpenTelemetry:', err)
+    } finally {
+        process.exit(0) // Finaliza o processo de maneira limpa
+    }
+});
